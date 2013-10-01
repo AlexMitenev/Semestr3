aeaf09e3fedef791ddd7a0cc0036a05d670413c3
Alex Mitenev <xawu@yandex.ru>
2013-10-01 14:55:31 +0400

    add tests

--- sem3/testMail/Program.fs
+++ sem3/testMail/Program.fs
@@ -1,4 +1,7 @@
-﻿open System.Text.RegularExpressions
+﻿open System
+open NUnit.Framework
+open FsUnit
+open System.Text.RegularExpressions
 
 let mailTest expr = 
     let beforeAt = "^[0-9a-zA-Z!#$%&'*+-/=?^_`{|}~]{2,64}@"
@@ -12,15 +15,36 @@ let test string =
         printf "%s" "error in "
         printfn "%s" string
 
-test "AlexMitenev@gmail.com"
-test "AlexMitenev@gmail.c"
-test "AlexMitenev@gmail"
-test "AlexMitenev"
-test "AlexMitenev@gmail:com"
-test "lolo.ayayay.lolo@lolo.lolo"
-test "A@t.ysdf"
-test "AlexMitenev@gmail.comsdfsdfsdf"
-test "!!!@gmail.cot"
-test "l!$l@gmail.com"
-test "VvV@gmail.com"
-test "AlexMitenev@gmail.lolo.rere.lala.com"
+[<TestFixture>] 
+type ``Good tests`` ()=
+
+    [<Test>] member x.
+     ``standart email`` ()=
+            mailTest "AlexMitenev@gmail.com" |> should be True
+
+    [<Test>] member x.
+     ``enother standart email`` ()=
+            mailTest "VvV@gmail.com" |> should be True
+
+    [<Test>] member x.
+     ``email with 2 domain`` ()=
+            mailTest "lolo.ayayay.lolo@lolo.lolo" |> should be True
+
+[<TestFixture>] 
+type ``Bad emails`` ()=
+
+    [<Test>] member x.
+     ``1 char in domain top level`` ()=
+            mailTest "AlexMitenev@gmail.c" |> should be False
+
+    [<Test>] member x.
+     ``colon before domain top level`` ()=
+            mailTest "AlexMitenev@gmail:com" |> should be False
+
+    [<Test>] member x.
+     ``email without domain`` ()=
+            mailTest "AlexMitenev@gmail" |> should be False
+
+    [<Test>] member x.
+     ``short email`` ()=
+            mailTest "A@t.ysdf" |> should be False
