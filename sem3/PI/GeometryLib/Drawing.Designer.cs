a1852befd97d393cb3317b32162462722a3f4f14
Alex Mitenev <alexmitenev@gmail.com>
2013-10-05 00:30:56 +0400

    add first versiom geometry library

---
+++ sem3/PI/GeometryLib/Drawing.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace IsRightTrangles
+{
+    partial class Drawing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawIt = new System.Windows.Forms.Button();
+            this.textValue = new System.Windows.Forms.TextBox();
+            this.textY3 = new System.Windows.Forms.TextBox();
+            this.textX3 = new System.Windows.Forms.TextBox();
+            this.textY2 = new System.Windows.Forms.TextBox();
+            this.textX2 = new System.Windows.Forms.TextBox();
+            this.textY1 = new System.Windows.Forms.TextBox();
+            this.textX1 = new System.Windows.Forms.TextBox();
+            this.labelX1 = new System.Windows.Forms.Label();
+            this.labelY1 = new System.Windows.Forms.Label();
+            this.labelX2 = new System.Windows.Forms.Label();
+            this.labelY2 = new System.Windows.Forms.Label();
+            this.labelX3 = new System.Windows.Forms.Label();
+            this.labelY3 = new System.Windows.Forms.Label();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawIt
+            // 
+            this.DrawIt.Location = new System.Drawing.Point(542, 347);
+            this.DrawIt.Name = "DrawIt";
+            this.DrawIt.Size = new System.Drawing.Size(75, 23);
+            this.DrawIt.TabIndex = 0;
+            this.DrawIt.Text = "DrawIt";
+            this.DrawIt.UseVisualStyleBackColor = true;
+            this.DrawIt.Click += new System.EventHandler(this.DrawIt_Click);
+            // 
+            // textValue
+            // 
+            this.textValue.Enabled = false;
+            this.textValue.Location = new System.Drawing.Point(542, 310);
+            this.textValue.Name = "textValue";
+            this.textValue.Size = new System.Drawing.Size(75, 20);
+            this.textValue.TabIndex = 1;
+            // 
+            // textY3
+            // 
+            this.textY3.Location = new System.Drawing.Point(582, 234);
+            this.textY3.Name = "textY3";
+            this.textY3.Size = new System.Drawing.Size(35, 20);
+            this.textY3.TabIndex = 2;
+            // 
+            // textX3
+            // 
+            this.textX3.Location = new System.Drawing.Point(582, 208);
+            this.textX3.Name = "textX3";
+            this.textX3.Size = new System.Drawing.Size(35, 20);
+            this.textX3.TabIndex = 3;
+            // 
+            // textY2
+            // 
+            this.textY2.Location = new System.Drawing.Point(582, 182);
+            this.textY2.Name = "textY2";
+            this.textY2.Size = new System.Drawing.Size(35, 20);
+            this.textY2.TabIndex = 4;
+            // 
+            // textX2
+            // 
+            this.textX2.Location = new System.Drawing.Point(582, 156);
+            this.textX2.Name = "textX2";
+            this.textX2.Size = new System.Drawing.Size(35, 20);
+            this.textX2.TabIndex = 5;
+            // 
+            // textY1
+            // 
+            this.textY1.Location = new System.Drawing.Point(582, 130);
+            this.textY1.Name = "textY1";
+            this.textY1.Size = new System.Drawing.Size(34, 20);
+            this.textY1.TabIndex = 6;
+            // 
+            // textX1
+            // 
+            this.textX1.Location = new System.Drawing.Point(582, 104);
+            this.textX1.Name = "textX1";
+            this.textX1.Size = new System.Drawing.Size(35, 20);
+            this.textX1.TabIndex = 7;
+            // 
+            // labelX1
+            // 
+            this.labelX1.AutoSize = true;
+            this.labelX1.Location = new System.Drawing.Point(558, 111);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(18, 13);
+            this.labelX1.TabIndex = 8;
+            this.labelX1.Text = "x1";
+            // 
+            // labelY1
+            // 
+            this.labelY1.AutoSize = true;
+            this.labelY1.Location = new System.Drawing.Point(558, 137);
+            this.labelY1.Name = "labelY1";
+            this.labelY1.Size = new System.Drawing.Size(18, 13);
+            this.labelY1.TabIndex = 9;
+            this.labelY1.Text = "y1";
+            // 
+            // labelX2
+            // 
+            this.labelX2.AutoSize = true;
+            this.labelX2.Location = new System.Drawing.Point(558, 163);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(18, 13);
+            this.labelX2.TabIndex = 10;
+            this.labelX2.Text = "x2";
+            // 
+            // labelY2
+            // 
+            this.labelY2.AutoSize = true;
+            this.labelY2.Location = new System.Drawing.Point(558, 189);
+            this.labelY2.Name = "labelY2";
+            this.labelY2.Size = new System.Drawing.Size(18, 13);
+            this.labelY2.TabIndex = 11;
+            this.labelY2.Text = "y2";
+            // 
+            // labelX3
+            // 
+            this.labelX3.AutoSize = true;
+            this.labelX3.Location = new System.Drawing.Point(558, 215);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(18, 13);
+            this.labelX3.TabIndex = 12;
+            this.labelX3.Text = "x3";
+            // 
+            // labelY3
+            // 
+            this.labelY3.AutoSize = true;
+            this.labelY3.Location = new System.Drawing.Point(558, 241);
+            this.labelY3.Name = "labelY3";
+            this.labelY3.Size = new System.Drawing.Size(18, 13);
+            this.labelY3.TabIndex = 13;
+            this.labelY3.Text = "y3";
+            // 
+            // labelValue
+            // 
+            this.labelValue.AutoSize = true;
+            this.labelValue.Location = new System.Drawing.Point(558, 294);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(43, 13);
+            this.labelValue.TabIndex = 14;
+            this.labelValue.Text = "Is Right";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(474, 313);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Drawing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(629, 409);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.labelY3);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.labelY2);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.labelY1);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.textX1);
+            this.Controls.Add(this.textY1);
+            this.Controls.Add(this.textX2);
+            this.Controls.Add(this.textY2);
+            this.Controls.Add(this.textX3);
+            this.Controls.Add(this.textY3);
+            this.Controls.Add(this.textValue);
+            this.Controls.Add(this.DrawIt);
+            this.Name = "Drawing";
+            this.Text = "Drawing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawIt;
+        private System.Windows.Forms.TextBox textValue;
+        private System.Windows.Forms.TextBox textY3;
+        private System.Windows.Forms.TextBox textX3;
+        private System.Windows.Forms.TextBox textY2;
+        private System.Windows.Forms.TextBox textX2;
+        private System.Windows.Forms.TextBox textY1;
+        private System.Windows.Forms.TextBox textX1;
+        private System.Windows.Forms.Label labelX1;
+        private System.Windows.Forms.Label labelY1;
+        private System.Windows.Forms.Label labelX2;
+        private System.Windows.Forms.Label labelY2;
+        private System.Windows.Forms.Label labelX3;
+        private System.Windows.Forms.Label labelY3;
+        private System.Windows.Forms.Label labelValue;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ sem3/PI/GeometryLib/Drawing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IsRightTrangles
+{
+    public partial class Drawing : Form
+    {
+        int x1, y1, x2, y2, x3, y3;
+
+        public Drawing()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawIt_Click(object sender, EventArgs e)
+        {
+            x1 = Convert.ToInt32(textX1.Text);
+            y1 = Convert.ToInt32(textY1.Text);
+            x2 = Convert.ToInt32(textX2.Text);
+            y2 = Convert.ToInt32(textY2.Text);
+            x3 = Convert.ToInt32(textX3.Text);
+            y3 = Convert.ToInt32(textY3.Text);
+
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Point p1 = new Point(x1, y1);
+            Point p2 = new Point(x2, y2);
+            Point p3 = new Point(x3, y3);
+            Pen pen = new Pen(Color.Black);
+            e.Graphics.DrawLine(pen, p1, p2);
+            e.Graphics.DrawLine(pen, p2, p3);
+            e.Graphics.DrawLine(pen, p3, p1);
+
+            textValue.Text = Lib.RightTrangle.IsRightTrangle(x1, y1, x2, y2, x3, y3).ToString();
+        }
+
+    }
+}
