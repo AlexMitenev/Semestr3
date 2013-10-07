5858ad86f3e45adb1748e5e6c0e8f53643637898
Alex Mitenev <alexmitenev@gmail.com>
2013-10-07 20:42:52 +0400

    update Tree homework

--- sem3/Haskell/Tree.hs
+++ sem3/Haskell/Tree.hs
@@ -1,24 +1,27 @@
 data BT a = N a (BT a) (BT a) | E deriving Show
 
-myTree = (N 4 (N 3 E E) (N 5 E E))
-
 insert E n = N n E E
 insert (N a l r) n 
 	| n < a = N a (insert l n) r
 	| n > a = N a l (insert r n)
 	| True = N a l r
 
-find E n = False
+find E _ = False
 find (N a l r) n 
 	| a == n = True
 	| n > a = find r n
-	| n < a = find l n
+	| True = find l n
 
 isBT E = True
-isBT (N n E E) = True
+isBT (N _ E E) = True
 isBT (N n l@(N a _ _) r@(N b _ _))
 	| n < a || n > b = False
 	| True = isBT l && isBT r
 
 elements E = []
 elements (N a l r) = elements l ++ [a] ++ elements r
+
+isEqual E E = True
+isEqual (N n1 l1 r1) (N n2 l2 r2) = (n1 == n2)      && 
+									 isEqual l1 l2  && 
+									 isEqual r1 r2
