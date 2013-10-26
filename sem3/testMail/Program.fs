b297e7ea5ebbfec9961d859fa4adc05fe460b209
Alex Mitenev <xawu@yandex.ru>
2013-10-27 00:00:40 +0400

    update email: now all tests work

--- sem3/testMail/Program.fs
+++ sem3/testMail/Program.fs
@@ -4,47 +4,55 @@ open FsUnit
 open System.Text.RegularExpressions
 
 let mailTest expr = 
-    let beforeAt = "^[0-9a-zA-Z!#$%&'*+-/=?^_`{|}~]{2,64}@"
-    let afterAt = "[-0-9A-Za-z_^\.]{2,192}\.[a-zA-Z]{2,6}$"
+    let beforeAt = "^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@"
+    let afterAt = "[-0-9A-Za-z_^\.]{1,192}\.[a-zA-Z]{2,6}$"
+    let doubleDot = new Regex("\.\.")
     let sum = beforeAt + afterAt
     let reg = new Regex(sum)
-    reg.IsMatch(expr)
-
-let test string =
-    if not (mailTest string) then 
-        printf "%s" "error in "
-        printfn "%s" string
+    reg.IsMatch(expr) &&  not <| doubleDot.IsMatch(expr)
 
 [<TestFixture>] 
 type ``Good tests`` ()=
 
     [<Test>] member x.
-     ``standart email`` ()=
-            mailTest "AlexMitenev@gmail.com" |> should be True
+     ``short email`` ()=
+            mailTest "a@b.cc" |> should be True
 
     [<Test>] member x.
      ``enother standart email`` ()=
-            mailTest "VvV@gmail.com" |> should be True
+            mailTest "victor.polozov@mail.ru" |> should be True
 
     [<Test>] member x.
-     ``email with 2 domain`` ()=
-            mailTest "lolo.ayayay.lolo@lolo.lolo" |> should be True
+     ``.info domen`` ()=
+            mailTest "my@domain.info" |> should be True
+
+    [<Test>] member x.
+     ``simbol domen`` ()=
+            mailTest "_.1@mail.com" |> should be True
+  
+    [<Test>] member x.
+     ``long domain`` ()=
+            mailTest "coins_department@hermitage.museum" |> should be True
 
 [<TestFixture>] 
 type ``Bad emails`` ()=
 
     [<Test>] member x.
      ``1 char in domain top level`` ()=
-            mailTest "AlexMitenev@gmail.c" |> should be False
+            mailTest "a@b.c" |> should be False
 
     [<Test>] member x.
-     ``colon before domain top level`` ()=
-            mailTest "AlexMitenev@gmail:com" |> should be False
+     ``double dot domain`` ()=
+            mailTest "a..b@mail.ru" |> should be False
 
     [<Test>] member x.
-     ``email without domain`` ()=
-            mailTest "AlexMitenev@gmail" |> should be False
+     ``domain without body`` ()=
+            mailTest ".a@mail.ru" |> should be False
 
     [<Test>] member x.
-     ``short email`` ()=
-            mailTest "A@t.ysdf" |> should be False
+     ``long domain top level`` ()=
+            mailTest "yo@domain.somedomain" |> should be False
+
+    [<Test>] member x.
+     ``only one num in body`` ()=
+            mailTest "1@mail.ru" |> should be False
