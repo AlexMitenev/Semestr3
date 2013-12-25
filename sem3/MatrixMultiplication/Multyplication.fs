c1c831af366c688242b2e4c01533e0c792ba5647
Alex Mitenev <xawu@yandex.ru>
2013-12-25 16:28:01 +0400

    add experiments for multythreading

---
+++ sem3/MatrixMultiplication/Multyplication.fs
@@ -0,0 +1,36 @@
+﻿// Asinc Matrix Multiplication
+//Alex Mitenev
+//271 group 
+
+module Multyplication
+
+let mult (a:int[,]) (b:int[,]) (th : int) =
+    let stringsA = Array2D.length1 a
+    let columnsA = Array2D.length2 a
+    let stringsB = Array2D.length1 b
+    let columnsB = Array2D.length2 b
+    let result = Array2D.create stringsA columnsB 0
+        
+    let oneAsyncMul l = 
+        [|for i in l ->
+              async{
+                  for j in 0 .. columnsB - 1 do
+                     for k in 0 .. columnsA - 1 do
+                         result.[i, j] <- result.[i, j] + a.[i, k] * b.[k, j]
+              }|]
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> ignore
+
+    // make list of lists for process who run parallel 
+    let makeList (l : int []) ths=
+        let len = l.Length
+        [for i in 0..ths..len - 1 -> if (i < ths * (len / ths)) 
+                                     then l.[i..i + ths - 1] 
+                                     else l.[i..len - 1]] 
+
+    let list = makeList [|0..stringsA - 1|] th
+
+    List.iter oneAsyncMul list
+
+    result
---
+++ sem3/MatrixMultiplication/TestPrint.fs
@@ -0,0 +1,37 @@
+﻿module TestPrint
+
+open System.Diagnostics
+
+let rnd = new System.Random()
+let timeCounter = new Stopwatch()
+
+let print m1 m2 threads =
+    printfn "%s" "---------matrix1------------"
+    printfn "%A" m1
+
+    printfn "%s" "---------matrix2------------"
+    printfn "%A" m2
+
+    printfn "%s" "---------result-------------"
+    printfn "%A" <| Multyplication.mult m1 m2 threads
+
+let CreateRandomMatrix maxNumber =
+    Array2D.init 1000 1000 (fun _ _ -> rnd.Next(maxNumber))
+
+let TestTime m1 m2 threads =
+    timeCounter.Start()
+    Multyplication.mult m1 m2 threads |> ignore
+    timeCounter.Stop()
+    printfn "%d threads for matrix mult 1000x1000: %A" threads timeCounter.Elapsed
+    timeCounter.Reset()
+
+let Experiment () =
+    let m1 = CreateRandomMatrix 5
+    let m2 = CreateRandomMatrix 5
+    List.iter (TestTime m1 m2) [1..16]
+
+
+Experiment()
+
+
+
